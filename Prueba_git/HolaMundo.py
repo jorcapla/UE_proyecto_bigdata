733e9ec941b965c2e0ffa6cb9cd911bce8b3c763
jorcapla <jorcapla@gmail.com>
2021-11-09 09:01:25 +0100

    He creado la función sumar

--- Prueba_git/HolaMundo.py
+++ Prueba_git/HolaMundo.py
@@ -1,3 +1,6 @@
 # Autor: Jorge Capel
 # Descripcion : Ejemplo de programa python para imprimir hola mundo en pantalla
-print("Hola Mundo");
+def sumar(a,b):
+    return a +b 
+resultado = sumar(5,6)
+print(resultado)
