36a2d5578960c3a6d5dfcece93b24623dd3b1e60
jorcapla <jorcapla@gmail.com>
2021-12-20 11:59:17 +0100

    Añado ejemplos de simulador de datos para contenedores

--- Simulador/scripts/generador_datos.py
+++ Simulador/scripts/generador_datos.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 from subprocess import call
 from time import sleep
-from Products import ProductsSimulation 
+from ProductsSimulation import ProductsSimulation 
+from ContenedorSimulation import ContenedorSimulation 
 import os
 
 IOT_AGENT_URL = os.getenv('IOT_AGENT_URL', 'http://localhost:7896/iot/d')
@@ -9,6 +10,10 @@ IOT_AGENT_KEY = os.getenv('IOT_AGENT_KEY', '4jggokgpepnvsb2uv4s40d5911')
 
 i=0
 while 1==1:
+
+
+  contenedor = ContenedorSimulation(IOT_AGENT_URL,IOT_AGENT_KEY)
+  contenedor.sendData()
   ProductsSimulation.sendData(IOT_AGENT_URL,IOT_AGENT_KEY)
   i+=1
  
